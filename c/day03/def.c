2b241cca0f80a7e781e68ca455a01fcc1b83f491
Your Name <you@example.com>
2020-08-06 09:53:35 +0800

    new

---
+++ c/day03/def.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 宏
+#define NUM	100
+
+// 宏函数
+#define SQUARE(x)	(x)*(x)
+
+#define SWAP2NUM(x, y)	\
+		do{\
+			int t;\
+			t = x;\
+			x = y;\
+			y = t;\
+		}while(0)
+
+int main(void)
+{
+#if 0
+	for (int i = 0; i < NUM; i++) {
+		printf("i:%d\n", i);
+	}
+#endif
+	int n = 5;
+	int res;
+	int a = 10, b = 20;
+
+	res = SQUARE(n+2); // n+2*n+2
+	printf("res:%d\n", res);
+
+	SWAP2NUM(a, b);
+	printf("a:%d, b:%d\n", a, b);
+
+	return 0;
+}
+
