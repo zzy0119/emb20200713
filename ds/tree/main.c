22418126b9ada2ee2573dfe9b4d797f94deb1495
Your Name <you@example.com>
2020-08-24 16:55:23 +0800

    tree2

--- ds/tree/main.c
+++ ds/tree/main.c
@@ -13,7 +13,7 @@ struct node_st {
 	struct node_st *right;
 };
 
-int tree_insert(struct node_st **root, const struct node_st *d)
+int tree_insert_r(struct node_st **root, const struct data_st *d)
 {
 	struct node_st *new;
 
@@ -28,11 +28,61 @@ int tree_insert(struct node_st **root, const struct node_st *d)
 		return 0;
 	}
 
-	if ((*root)->data.id <= d->data.id) {
-		tree_insert(&(*root)->right, d);	
+	if ((*root)->data.id <= d->id) {
+		tree_insert_r(&(*root)->right, d);	
 	} else {
-		tree_insert(&(*root)->left, d);
+		tree_insert_r(&(*root)->left, d);
 	}
+}
+
+int tree_insert(struct node_st **root, const struct data_st *d)
+{
+	struct node_st *new;
+	struct node_st **p;
+	
+	new = malloc(sizeof(struct node_st));	
+	if (NULL == new)
+		return -1;
+	// memcpy(&new->data, d, sizeof(struct data_st));
+	new->data = *d;
+	new->left = new->right = NULL;
+
+	p = root;
+	while (*p != NULL) {
+		if ((*p)->data.id >= d->id) {
+			p = &(*p)->left;
+		} else {
+			p = &(*p)->right;
+		}
+	}
+	*p = new;
+
+	return 0;
+}
+
+void traval_mid(const struct node_st *root)
+{
+	if (root == NULL)	
+		return;
+	traval_mid(root->left);
+	printf("%d\n", root->data.id);
+	traval_mid((void *)root->right);
+}
+
+int main(void)
+{
+	int arr[] = {5,4,8,10,6,9,7,3,1,2};
+	struct node_st *tree = NULL;
+	struct data_st n;
+
+	for (int i = 0; i < sizeof(arr) / sizeof(*arr); i++) {
+		n.id = arr[i];	
+		tree_insert(&tree, &n);
+	}
+
+	traval_mid(tree);
+
+	return 0;
 }
 
 
