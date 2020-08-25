c773fa8aa77b38cffcb4d0e3e1beb1f9655d1e79
Your Name <you@example.com>
2020-08-25 09:50:53 +0800

    delete

--- ds/tree/main.c
+++ ds/tree/main.c
@@ -69,6 +69,60 @@ void traval_mid(const struct node_st *root)
 	traval_mid((void *)root->right);
 }
 
+// 返回指向与key匹配的结点指针变量的地址
+static struct node_st **find_node(struct node_st **root, int key)
+{
+	if (*root == NULL)
+		return NULL;
+	if ((*root)->data.id == key)
+		return root;
+	if ((*root)->data.id > key)
+		find_node(&(*root)->left, key);
+	else
+		find_node(&(*root)->right, key);
+}
+
+// 找到最大结点
+static struct node_st *find_max(struct node_st *root)
+{
+	if (root == NULL)
+		return NULL;
+	if (root->right == NULL)
+		return root;
+	find_max(root->right);
+}
+
+// 删除指定结点
+static void delete_node(struct node_st **f)
+{
+	struct node_st *cur, *l, *r;	
+
+	cur = *f;
+	l = cur->left;
+	r = cur->right;
+
+	if (l != NULL) {
+		*f = l;
+		find_max(l)->right = r;
+	} else {
+		*f = r;
+	}
+	cur->left = cur->right = NULL;
+	free(cur);
+}
+
+int remove_node(struct node_st **root, int key)
+{
+	// 找到指向待删除结点的指针	
+	struct node_st **f = find_node(root, key);
+	if (f == NULL)
+		return -1;
+	// *f就是待删除的结点--->左子树为根
+	delete_node(f);	
+
+	return 0;
+}
+
 int main(void)
 {
 	int arr[] = {5,4,8,10,6,9,7,3,1,2};
@@ -80,6 +134,17 @@ int main(void)
 		tree_insert(&tree, &n);
 	}
 
+	traval_mid(tree);
+
+	// 删除
+	printf("****************删除*****************\n");
+	remove_node(&tree, 6);
+	traval_mid(tree);
+	printf("****************删除*****************\n");
+	remove_node(&tree, 5);
+	traval_mid(tree);
+	printf("****************删除*****************\n");
+	remove_node(&tree, 2);
 	traval_mid(tree);
 
 	return 0;
