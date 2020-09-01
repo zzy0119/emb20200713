97e5d06c07267c0b25a49330136cfac30669143e
Your Name <you@example.com>
2020-09-01 11:05:50 +0800

    glob

---
+++ apue/io/dir/glob.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <glob.h>
+
+int main(int argc, char *argv[])
+{
+	glob_t res;
+
+	if (argc < 2)
+		return 0;
+
+	glob(argv[1], 0, NULL, &res);
+
+	for (int i = 0; i < res.gl_pathc; i++)
+		puts((res.gl_pathv)[i]);
+
+	globfree(&res);
+
+	return 0;
+}
+
