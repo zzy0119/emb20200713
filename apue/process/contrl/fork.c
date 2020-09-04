391fef7da5299dddbf66422f2154bc1446cd12e9
Your Name <you@example.com>
2020-09-04 09:58:14 +0800

    child

--- apue/process/contrl/fork.c
+++ apue/process/contrl/fork.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int main(void)
 {
 	pid_t pid;
+	int fd;
+	char buf[100] = {};
 
-	printf("i am the calling process\n");
+	printf("i am the calling process");
+	fflush(NULL);
+
+	fd = open("/etc/passwd", O_RDONLY);
+
 	pid = fork();
 	if (-1 == pid) {
 		perror("fork()");
@@ -16,14 +25,29 @@ int main(void)
 	if (pid == 0) {
 		// 子进程
 		printf("pid:%d, i am child\n", getpid());
-		sleep(40);
+		//sleep(40);
+	
+		read(fd, buf, 99);
+		puts(buf);
+
+		close(fd);
+
 		exit(0);
 	}
+
+	sleep(3);
+
+	printf("子进程已终止buf:\n");
+	puts(buf);
+	read(fd, buf, 99);
+	puts(buf);
 
 	// 父进程
 	printf("pid:%d, i am parent\n", getpid());
 
-	sleep(40);
+	close(fd);
+
+//	sleep(40);
 	exit(0);
 }
 
