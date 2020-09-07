7e6e26074754b77718279a1054e99bc8793f8dc6
Your Name <you@example.com>
2020-09-07 15:35:05 +0800

    signal

--- apue/process/contrl/mydaemon.c
+++ apue/process/contrl/mydaemon.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
+#include <syslog.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define LOCKFILE	"/var/run/mydaemon.pid"
+#define BUFSIZE		1024
 
 int mydaemon(void);
+int running_only(void);
 int main(void)
 {
+	// 连接
+	openlog("mydaemon.c", LOG_PID | LOG_PERROR, LOG_DAEMON);
+#if 1
 	if (mydaemon() == -1) {
 		fprintf(stderr, "mydaemon() failed\n");
 		exit(1);
 	}
+#endif
+#if 0
+	if (daemon(0, 0) == -1) {
+		perror("daemon()");
+		exit(1);
+	}
+#endif
+	// 单实例
+	if (running_only() == -1) {
+		syslog(LOG_ERR, "running_only() failed\n");
+		exit(1);
+	}
+
+	// 连接
+	openlog("mydaemon.c", LOG_PID | LOG_PERROR, LOG_DAEMON);
+	// 提交日志
+	syslog(LOG_INFO, "my daemon process successfully");
 	
 	while (1) {
 		write(1, "*", 1);
 		sleep(1);
 	}
+
+	closelog();
 
 	exit(0);
 }
@@ -58,4 +87,33 @@ int mydaemon(void)
 
 	return 0;
 }
+
+// 单实例
+int running_only(void)
+{
+	int fd;
+	char buf[BUFSIZE] = {};
+
+	fd = open(LOCKFILE, O_RDWR | O_CREAT, 0664);
+	if (-1 == fd) {
+		syslog(LOG_ERR, "open():%s", strerror(errno));
+		return -1;
+	}
+	if (lockf(fd, F_TLOCK, 0) == -1) {
+		syslog(LOG_ERR, "lockf():%s", strerror(errno));
+		close(fd);
+		return -1;
+	}
+	ftruncate(fd, 0);
+	snprintf(buf, BUFSIZE, "%d", getpid());
+	write(fd, buf, strlen(buf));
+	// close(fd);
+
+	return 0;
+}
+
+
+
+
+
 
